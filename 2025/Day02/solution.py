25e002fb5e464ce3533b7c005616967cfada98df
Julian <167174811+JBTastic@users.noreply.github.com>
2025-12-02 17:50:14 +0100

    Day 2 Part 2

--- 2025/Day02/solution.py
+++ 2025/Day02/solution.py
@@ -30,7 +30,7 @@ def find_invalid_ids_1(start: int, end: int) -> list[int]:
 # find invalid IDs and add them up
 def part_1():
     # read input from file
-    with open("2.1_input.txt", "r") as input:
+    with open("input.txt", "r") as input:
         data = input.read().split(",")
         
         invalid_ids = []
@@ -39,15 +39,61 @@ def part_1():
             # id ranges
             id_range = str(id_ranges).split("-")
             
-            print(f"ID ranges: {id_range}")
+            # print(f"ID ranges: {id_range}")
             
             invalid_ids.extend(find_invalid_ids_1(int(id_range[0]), int(id_range[1])))
             
             # print(f"Invalid IDs: {invalid_ids}")
-        print(f"Sum of invalid IDs: {sum(invalid_ids)}")
+        print(f"Sum of invalid IDs Part 1: {sum(invalid_ids)}")
 
 
+def find_invalid_ids_2(start: int, end: int) -> list[int]:
+    invalid_ids = []
+    
+    for num in range(start, end + 1):
+        num_string = str(num)
+        num_length = len(num_string)
+        is_invalid = False
+
+        # Try block lengths: 1 to half of the total length
+        for block_len in range(1, num_length // 2 + 1):
+            
+            # Length must be a multiple of the block length
+            if num_length % block_len != 0:
+                continue
+
+            block = num_string[:block_len]
+            repeats = num_length // block_len
+
+            if block * repeats == num_string:
+                is_invalid = True
+                break
+
+        if is_invalid:
+            invalid_ids.append(num)
+
+    return invalid_ids
+
+
+
+def part_2():
+    with open("input.txt", "r") as input:
+        data = input.read().split(",")
         
+        invalid_ids = []
+        
+        for id_ranges in data:
+            # id ranges
+            id_range = str(id_ranges).split("-")
+            
+            # print(f"ID ranges: {id_range}")
+            
+            invalid_ids.extend(find_invalid_ids_2(int(id_range[0]), int(id_range[1])))
+            
+            # print(f"Invalid IDs: {invalid_ids}")
+        print(f"Sum of invalid IDs Part 2: {sum(invalid_ids)}")
+
+
 if __name__ == "__main__":
     part_1()
-    # part_2()
+    part_2()
